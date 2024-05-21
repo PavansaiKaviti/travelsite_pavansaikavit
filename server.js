6f6286437370e71740790720d69b015750c54a32
PavansaiKaviti <pavan94901@gmail.com>
2024-05-21 13:03:44 -0500

    fixed server

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ const app = express();
 //!middlewares
 
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
